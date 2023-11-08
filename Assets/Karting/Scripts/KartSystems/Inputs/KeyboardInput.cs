3e7bff0d7c0d10a39de41f6c5cff60395f56dcc0
spie51 <stan173@outlook.com>
2023-11-07 22:06:42 -0500

    vr steering

--- Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
+++ Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -1,19 +1,27 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace KartGame.KartSystems {
 
     public class KeyboardInput : BaseInput
     {
+        public HingeJoint wheel;
         public string TurnInputName = "Horizontal";
         public string AccelerateButtonName = "Accelerate";
         public string BrakeButtonName = "Brake";
 
         public override InputData GenerateInput() {
+
+            // Wheel Rotation to Turn Output Logic
+            float steeringNormal = Mathf.InverseLerp(-0.35f, 0.35f, wheel.transform.localRotation.x);
+            float steeringRange = -1 * Mathf.Lerp(-1, 1, steeringNormal);
             return new InputData
             {
                 Accelerate = Input.GetButton(AccelerateButtonName),
                 Brake = Input.GetButton(BrakeButtonName),
-                TurnInput = Input.GetAxis("Horizontal")
+                TurnInput = (Mathf.Abs(steeringRange) < 0.2f) ? Input.GetAxis("Horizontal") : steeringRange
+
             };
         }
     }
